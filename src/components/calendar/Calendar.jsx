3e493f983f657eca18da840adecb21b06f288b09
taras <taraslychynets@icloud.com>
2022-01-31 16:03:49 +0200

    calendar

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -3,13 +3,43 @@ import React, { Component } from "react";
 import Navigation from "./../navigation/Navigation";
 import Week from "../week/Week";
 import Sidebar from "../sidebar/Sidebar";
-import events from "../../gateway/events";
+import {
+  fetchEventsCalendare,
+  createEvent,
+  deleteEvent,
+} from "../../gateway/events";
 
 import "./calendar.scss";
+import Modal from "../modal/Modal";
 
 class Calendar extends Component {
   state = {
-    events,
+    events: [],
+  };
+
+  componentDidMount() {
+    this.fetchEvents();
+  }
+
+  fetchEvents = () => {
+    fetchEventsCalendare().then((eventsCalendare) =>
+      this.setState({
+        events: eventsCalendare,
+      })
+    );
+  }
+
+  handleSubmit = (event) => {
+    event.id = Math.random();
+    // this.setState({
+    //   events: [...this.state.events, event],
+    // });
+
+    createEvent(event).then(() => this.fetchEvents());
+  };
+
+  handleEventDelete = (id) => {
+    deleteEvent(id).then(() => this.fetchEvents());
   };
 
   render() {
@@ -20,8 +50,18 @@ class Calendar extends Component {
         <Navigation weekDates={weekDates} />
         <div className="calendar__body">
           <div className="calendar__week-container">
+            {this.props.visible && (
+              <Modal
+                onClose={this.props.onClose}
+                onSubmit={this.handleSubmit}
+              />
+            )}
             <Sidebar />
-            <Week weekDates={weekDates} events={this.state.events} />
+            <Week
+              weekDates={weekDates}
+              events={this.state.events}
+              onDelete={this.handleEventDelete}
+            />
           </div>
         </div>
       </section>
