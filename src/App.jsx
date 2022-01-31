77e967a24a65f0bd7b805023f61962782cbd253e
taras <taraslychynets@icloud.com>
2022-01-31 16:03:32 +0200

    app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,48 @@ import React, { Component } from "react";
 import Header from "./components/header/Header.jsx";
 import Calendar from "./components/calendar/Calendar.jsx";
 
-import { getWeekStartDate, generateWeekRange } from "../src/utils/dateUtils.js";
+import {
+  getWeekStartDate,
+  generateWeekRange,
+  setDay,
+} from "../src/utils/dateUtils.js";
 
 import "./common.scss";
 
 class App extends Component {
   state = {
     weekStartDate: new Date(),
+    visible: false,
+  };
+
+  handleCreateEvent = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleTodayDate = () => {
+    this.setState({
+      weekStartDate: new Date(),
+    });
+  };
+
+  handleLeft = () => {
+    this.setState({
+      weekStartDate: setDay(this.state.weekStartDate,false),
+    });
+  };
+
+  handleRight = () => {
+    this.setState({
+      weekStartDate: setDay(this.state.weekStartDate, true),
+    });
+  };
+
+  handleCloseModalEvent = () => {
+    this.setState({
+      visible: false,
+    });
   };
 
   render() {
@@ -17,8 +52,18 @@ class App extends Component {
 
     return (
       <>
-        <Header />
-        <Calendar weekDates={weekDates} />
+        <Header
+          handleCreateEvent={this.handleCreateEvent}
+          handleTodayDate={this.handleTodayDate}
+          handleLeft={this.handleLeft}
+          handleRight={this.handleRight}
+          date={this.state.weekStartDate}
+        />
+        <Calendar
+          weekDates={weekDates}
+          visible={this.state.visible}
+          onClose={this.handleCloseModalEvent}
+        />
       </>
     );
   }
