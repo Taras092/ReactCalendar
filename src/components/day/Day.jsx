7a39f8513553ba0b58dc45ae27625d5a5cd25f53
taras <taraslychynets@icloud.com>
2022-02-16 14:19:28 +0200

    review code

--- src/components/day/Day.jsx
+++ src/components/day/Day.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Hour from '../hour/Hour';
-import TimeLine from '../TimeLine/TimeLine';
 
 import './day.scss';
 
@@ -9,12 +8,8 @@ const Day = ({ dataDay, dayEvents, onDelete }) => {
     .fill()
     .map((val, index) => index);
 
-  // const isToday = dataDay === new Date().getDate();
-  // const isHour = hours.map(hour => new Date().getHours() === hour);
-
   return (
     <div className="calendar__day" data-day={dataDay}>
-      {/* {isToday && isHour ? <TimeLine /> : null} */}
       {hours.map(hour => {
         //getting all events from the day we will render
         const hourEvents = dayEvents.filter(event => event.dateFrom.getHours() === hour);
