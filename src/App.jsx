03e74ae857cfb6a3e3828c30a7c19acb9e00e15f
taras <taraslychynets@icloud.com>
2022-02-21 14:48:23 +0200

    used hooks

--- src/App.jsx
+++ src/App.jsx
@@ -6,64 +6,6 @@ import { getWeekStartDate, generateWeekRange, setDay } from '../src/utils/dateUt
 
 import './common.scss';
 
-// class App extends Component {
-//   state = {
-//     weekStartDate: new Date(),
-//     visible: false,
-//   };
-
-//   handleCreateEvent = () => {
-//     this.setState({
-//       visible: true,
-//     });
-//   };
-
-//   handleTodayDate = () => {
-//     this.setState({
-//       weekStartDate: new Date(),
-//     });
-//   };
-
-//   handleLeft = () => {
-//     this.setState({
-//       weekStartDate: setDay(this.state.weekStartDate, false),
-//     });
-//   };
-
-//   handleRight = () => {
-//     this.setState({
-//       weekStartDate: setDay(this.state.weekStartDate, true),
-//     });
-//   };
-
-//   handleCloseModalEvent = () => {
-//     this.setState({
-//       visible: false,
-//     });
-//   };
-
-//   render() {
-//     const { weekStartDate } = this.state;
-//     const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
-//     return (
-//       <>
-//         <Header
-//           onCreateEvent={this.handleCreateEvent}
-//           onTodayDate={this.handleTodayDate}
-//           onPrevMonth={this.handleLeft}
-//           onNextMonth={this.handleRight}
-//           date={this.state.weekStartDate}
-//         />
-//         <Calendar
-//           weekDates={weekDates}
-//           visible={this.state.visible}
-//           onClose={this.handleCloseModalEvent}
-//         />
-//       </>
-//     );
-//   }
-// }
-
 const App = () => {
   const [weekStartDate, setWeekStartDate] = useState(new Date());
   const [visible, setVisible] = useState(false);
@@ -77,11 +19,11 @@ const App = () => {
   };
 
   const handleLeft = () => {
-    setWeekStartDate(setDay(weekStartDate, false));
+    setWeekStartDate(new Date(setDay(weekStartDate, false)));
   };
 
   const handleRight = () => {
-    setWeekStartDate(setDay(weekStartDate, true));
+    setWeekStartDate(new Date(setDay(weekStartDate, true)));
   };
 
   const handleCloseModalEvent = () => {
@@ -98,10 +40,7 @@ const App = () => {
         onNextMonth={handleRight}
         date={weekStartDate}
       />
-      <Calendar 
-      weekDates={weekDates} 
-      visible={visible} 
-      onClose={handleCloseModalEvent} />
+      <Calendar weekDates={weekDates} visible={visible} onClose={handleCloseModalEvent} />
     </>
   );
 };
